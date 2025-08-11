6e04e97d7d5f858558ab77330457ee4b4a0cebd4
halymist <halymist@gmail.com>
2025-08-11 22:11:23 +0200

    yep

--- scripts/ItemDescription.gd
+++ scripts/ItemDescription.gd
@@ -5,17 +5,60 @@ extends Panel
 @export var agility: Label
 @export var luck: Label
 @export var armor: Label
-@export var effect: Label
+@export var effect: RichTextLabel
+
+# References to the stat containers for hiding/showing
+@onready var strength_container = strength.get_parent()
+@onready var stamina_container = stamina.get_parent()
+@onready var agility_container = agility.get_parent()
+@onready var luck_container = luck.get_parent()
+@onready var armor_container = armor.get_parent()
 
 func show_description(item_data: GameInfo.Item, mouse_position: Vector2 = Vector2.ZERO):
 	if item_data:
 		name_label.text = item_data.item_name
-		strength.text = str(item_data.strength)
-		stamina.text = str(item_data.constitution)
-		agility.text = str(item_data.dexterity)
-		luck.text = str(item_data.luck)
-		armor.text = str(item_data.armor)
-		effect.text = item_data.effect_description
+		
+		# Handle strength stat - hide if 0
+		if item_data.strength != 0:
+			strength.text = str(item_data.strength)
+			strength_container.visible = true
+		else:
+			strength_container.visible = false
+		
+		# Handle stamina stat - hide if 0
+		if item_data.constitution != 0:
+			stamina.text = str(item_data.constitution)
+			stamina_container.visible = true
+		else:
+			stamina_container.visible = false
+		
+		# Handle agility stat - hide if 0
+		if item_data.dexterity != 0:
+			agility.text = str(item_data.dexterity)
+			agility_container.visible = true
+		else:
+			agility_container.visible = false
+		
+		# Handle luck stat - hide if 0
+		if item_data.luck != 0:
+			luck.text = str(item_data.luck)
+			luck_container.visible = true
+		else:
+			luck_container.visible = false
+		
+		# Handle armor stat - hide if 0
+		if item_data.armor != 0:
+			armor.text = str(item_data.armor)
+			armor_container.visible = true
+		else:
+			armor_container.visible = false
+		
+		# Handle effect description - hide if empty
+		if item_data.effect_description != "":
+			effect.text = "[i]" + item_data.effect_description + "[/i]"
+			effect.visible = true
+		else:
+			effect.visible = false
 		
 		# Position the panel relative to mouse/hover position if provided
 		if mouse_position != Vector2.ZERO:
