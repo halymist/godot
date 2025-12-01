48657516b855f0c2f4ba844250fc05a816c50557
halymist <halymist@gmail.com>
2025-12-01 12:20:16 +0100

    better bullshit

--- scripts/SquareContent.gd
+++ scripts/SquareContent.gd
@@ -1,9 +1,9 @@
+@tool
 extends Control
 
-func _ready():
-	custom_minimum_size = Vector2(600, 600)
-
 func _process(_delta):
-	var h = get_parent().size.y
-	custom_minimum_size = Vector2(h, h)
-	scale = Vector2(h / 600.0, h / 600.0)
+	var parent = get_parent()
+	if parent:
+		var h = parent.size.y
+		custom_minimum_size.x = h
+		size.x = h
