f8afe5b5fed987bbf2eea85e5ee11fa92ce91cfd
halymist <halymist@gmail.com>
2025-12-02 15:37:51 +0100

    clickable vendor

---
+++ scripts/Utility.gd
@@ -0,0 +1,25 @@
+extends Control
+class_name Utility
+
+@export var target_panel: Panel
+
+@onready var click_button: Button = $ClickButton
+@onready var hover_area: ColorRect = $HoverArea
+
+signal utility_clicked(utility: Utility)
+
+func _ready():
+	click_button.button_up.connect(_on_button_pressed)
+	click_button.mouse_entered.connect(_on_mouse_entered)
+	click_button.mouse_exited.connect(_on_mouse_exited)
+
+func _on_mouse_entered():
+	hover_area.color = Color(1, 1, 1, 0.3)
+
+func _on_mouse_exited():
+	hover_area.color = Color(1, 1, 1, 0)
+
+func _on_button_pressed():
+	if target_panel:
+		target_panel.visible = true
+	utility_clicked.emit(self)
