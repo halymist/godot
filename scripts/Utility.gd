6a7bdde675dc2ae10eb0adefceb2386ee133265f
halymist <halymist@gmail.com>
2025-12-02 16:00:21 +0100

    vendors in different locations

--- scripts/Utility.gd
+++ scripts/Utility.gd
@@ -21,6 +21,11 @@ func _on_mouse_exited():
 
 func _on_button_pressed():
 	if target_panel:
+		# If this is the vendor panel, set the location-specific background
+		if target_panel.name == "VendorPanel" and target_panel.has_method("set_vendor_location"):
+			var location = GameInfo.current_player.location if GameInfo.current_player else 1
+			target_panel.set_vendor_location(location)
+		
 		target_panel.visible = true
 		GameInfo.set_current_panel(target_panel)
 	utility_clicked.emit(self)
--- scripts/VendorPanel.gd
+++ scripts/VendorPanel.gd
@@ -1,6 +1,17 @@
 @tool
 extends Panel
 
+@export var village1_texture: Texture
+@export var village2_texture: Texture
+@export var village3_texture: Texture
+@export var village4_texture: Texture
+@export var village5_texture: Texture
+
+func _ready():
+	if not Engine.is_editor_hint():
+		var location = GameInfo.current_player.location if GameInfo.current_player else 1
+		set_vendor_location(location)
+
 func _process(_delta):
 	_update_layout()
 
@@ -33,3 +44,25 @@ func _update_layout():
 	var bag_height = remaining_height * 0.2
 	player_bag_panel.size = Vector2(panel_width, bag_height)
 	player_bag_panel.position = Vector2(0, bg_height + items_height)
+
+func set_vendor_location(location: int):
+	var background_texture = get_node_or_null("VendorBackground/Background")
+	if not background_texture or not background_texture is TextureRect:
+		return
+	
+	# Assign the texture based on the location
+	var texture: Texture = null
+	match location:
+		1:
+			texture = village1_texture
+		2:
+			texture = village2_texture
+		3:
+			texture = village3_texture
+		4:
+			texture = village4_texture
+		5:
+			texture = village5_texture
+	
+	if texture:
+		background_texture.texture = texture
