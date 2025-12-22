9bf4b1e21d099883b088934012ada5b1fd148724
halymist <halymist@gmail.com>
2025-12-22 11:48:19 +0100

    better

--- scripts/BlacksmithPanel.gd
+++ scripts/BlacksmithPanel.gd
@@ -4,9 +4,9 @@ extends "res://scripts/UtilityPanel.gd"
 # BlacksmithPanel-specific functionality can be added here
 # For now, it uses all functionality from UtilityPanel
 
-@onready var blacksmith_slot = $UtilityPanel/Content/ItemAndStats/ItemSlotContainer/ItemSlot
-@onready var improved_stats_label = $UtilityPanel/Content/ItemAndStats/StatsContainer/ImprovedStats
-@onready var temper_button = $UtilityPanel/Content/TemperButtonContainer/TemperButton
+@export var blacksmith_slot: Control
+@export var improved_stats_label: Label
+@export var temper_button: Button
 
 const TEMPER_COST = 10
 
