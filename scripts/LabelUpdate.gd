d3dc3960bc0f84bcf2e3be73c04d5665a6823f64
halymist <halymist@gmail.com>
2025-12-25 22:43:45 +0100

    grid gap

--- scripts/LabelUpdate.gd
+++ scripts/LabelUpdate.gd
@@ -3,10 +3,7 @@ extends Control
 @export var player_name_label: Label
 @export var rank_label: Label
 @export var guild_label: Label
-@export var guild_icon: TextureRect
 @export var profession_label: Label
-@export var profession_icon: TextureRect
-@export var talent_points_label: Label
 @export var strength_label: Label
 @export var strength_aprox: Label
 @export var stamina_label: Label
@@ -18,18 +15,6 @@ extends Control
 @export var armor_label: Label
 
 func _ready():
-	# Load profession icon once on ready
-	if profession_icon:
-		var icon_path = GameInfo.get_profession_icon(GameInfo.current_player.profession)
-		if icon_path and ResourceLoader.exists(icon_path):
-			profession_icon.texture = load(icon_path)
-	
-	# Load guild icon once on ready
-	if guild_icon:
-		var icon_path = GameInfo.get_guild_icon(GameInfo.current_player.guild)
-		if icon_path and ResourceLoader.exists(icon_path):
-			guild_icon.texture = load(icon_path)
-	
 	stats_changed(GameInfo.get_player_stats())
 
 # Called when GameInfo current player is updated
@@ -39,10 +24,12 @@ func stats_changed(_stats: Dictionary):
 	rank_label.text = GameInfo.current_player.get_rank_name() + " (" + str(GameInfo.current_player.rank) + ")"
 	guild_label.text = GameInfo.current_player.get_guild_name()
 	profession_label.text = GameInfo.current_player.get_profession_name()
-	if talent_points_label:
-		talent_points_label.text = "TALENT POINTS: " + str(GameInfo.current_player.talent_points)
-	strength_label.text = "STRENGTH: " + str(int(total_stats.strength))
-	stamina_label.text = "STAMINA: " + str(int(total_stats.stamina))
-	agility_label.text = "AGILITY: " + str(int(total_stats.agility))
-	luck_label.text = "LUCK: " + str(int(total_stats.luck))
-	armor_label.text = "ARMOR: " + str(int(total_stats.armor))
+	strength_label.text = str(int(total_stats.strength))
+	strength_aprox.text = "(100)"
+	stamina_label.text = str(int(total_stats.stamina))
+	stamina_aprox.text = "(100)"
+	agility_label.text = str(int(total_stats.agility))
+	agility_aprox.text = "(100)"
+	luck_label.text = str(int(total_stats.luck))
+	luck_aprox.text = "(100)"
+	armor_label.text = str(int(total_stats.armor))
