1c02f2ef379dae13e40952e20ea1b0d547a3a3f3
halymist <halymist@gmail.com>
2026-01-17 13:46:00 +0100

    pre cleanup

--- scripts/CharacterInfoPanel.gd
+++ scripts/CharacterInfoPanel.gd
@@ -1,6 +1,6 @@
 extends Control
 
-# Character info panel for name, faction, VIP selection, and stat allocation
+# Character info panel for name, faction, and VIP selection
 @export var name_input: LineEdit
 @export var order_button: Button
 @export var guild_button: Button
@@ -9,21 +9,6 @@ extends Control
 @export var vip_no_button: Button
 @export var next_button: Button
 @export var back_button: Button
-
-# Stat allocation UI
-@export var strength_label: Label
-@export var stamina_label: Label
-@export var agility_label: Label
-@export var luck_label: Label
-@export var points_label: Label
-@export var strength_minus: Button
-@export var strength_plus: Button
-@export var stamina_minus: Button
-@export var stamina_plus: Button
-@export var agility_minus: Button
-@export var agility_plus: Button
-@export var luck_minus: Button
-@export var luck_plus: Button
 
 # Description labels
 @export var faction_desc_label: Label
@@ -44,13 +29,6 @@ var character_name: String = ""
 var faction: int = 1  # 1=Order, 2=Guild, 3=Companions (Order pre-selected)
 var is_vip: bool = false
 
-# Stat allocation
-var strength: int = 5
-var stamina: int = 5
-var agility: int = 5
-var luck: int = 5
-var points_remaining: int = 10
-
 signal next_pressed
 signal back_pressed
 
@@ -69,17 +47,6 @@ func _ready():
 	vip_yes_button.pressed.connect(_on_vip_selected.bind(true))
 	vip_no_button.pressed.connect(_on_vip_selected.bind(false))
 	
-	# Connect stat buttons
-	strength_minus.pressed.connect(_on_stat_changed.bind("strength", -1))
-	strength_plus.pressed.connect(_on_stat_changed.bind("strength", 1))
-	stamina_minus.pressed.connect(_on_stat_changed.bind("stamina", -1))
-	stamina_plus.pressed.connect(_on_stat_changed.bind("stamina", 1))
-	agility_minus.pressed.connect(_on_stat_changed.bind("agility", -1))
-	agility_plus.pressed.connect(_on_stat_changed.bind("agility", 1))
-	luck_minus.pressed.connect(_on_stat_changed.bind("luck", -1))
-	luck_plus.pressed.connect(_on_stat_changed.bind("luck", 1))
-	
-	_update_stat_display()
 	_update_descriptions()
 
 func _on_faction_selected(faction_id: int):
@@ -118,44 +85,6 @@ func _update_descriptions():
 		else:
 			vip_desc_label.text = no_vip_description
 
-func _on_stat_changed(stat_name: String, delta: int):
-	var current_value = get(stat_name)
-	var new_value = current_value + delta
-	
-	# Validate change
-	if delta > 0:
-		# Adding points - check if we have points remaining
-		if points_remaining <= 0:
-			return
-	else:
-		# Removing points - check if stat is above minimum (5)
-		if current_value <= 5:
-			return
-	
-	# Apply change
-	set(stat_name, new_value)
-	points_remaining -= delta
-	_update_stat_display()
-
-func _update_stat_display():
-	strength_label.text = str(strength)
-	stamina_label.text = str(stamina)
-	agility_label.text = str(agility)
-	luck_label.text = str(luck)
-	points_label.text = "Remaining: " + str(points_remaining)
-	
-	# Enable/disable minus buttons
-	strength_minus.disabled = strength <= 5
-	stamina_minus.disabled = stamina <= 5
-	agility_minus.disabled = agility <= 5
-	luck_minus.disabled = luck <= 5
-	
-	# Enable/disable plus buttons
-	strength_plus.disabled = points_remaining <= 0
-	stamina_plus.disabled = points_remaining <= 0
-	agility_plus.disabled = points_remaining <= 0
-	luck_plus.disabled = points_remaining <= 0
-
 func _on_next_pressed():
 	character_name = name_input.text.strip_edges()
 	
@@ -164,7 +93,6 @@ func _on_next_pressed():
 		return
 	
 	print("Character info complete: ", character_name, " Faction: ", faction, " VIP: ", is_vip)
-	print("Stats: STR:", strength, " STA:", stamina, " AGI:", agility, " LCK:", luck)
 	next_pressed.emit()
 
 func _on_back_pressed():
